daf54c29feb58892d79acb0d82e80e972f80c29b
Fxm <fxm_cs@163.com>
2025-05-24 16:44:56 +0800

    游戏背景图替换服务器

--- data/FishData2/WaterData.js
+++ data/FishData2/WaterData.js
@@ -11,8 +11,8 @@ export const WaterData = [
         // shallow: '浅滩',
         // obstacle: '障碍'
       },
-      image: '../../images/icons/stream.png',
-      backgroundImage: '/images/icons/stream.png',
+      image: 'https://anglertest.xyz/game/BG/stream.png',
+      backgroundImage: 'https://anglertest.xyz/game/BG/stream.png',
       fishProbabilities: {
         backwater: { FISH_01: 0.2, FISH_02: 0.2,  FISH_03: 0.2,  FISH_04: 0.2,  FISH_05: 0.2,NONE: 0 },
         slowmoving: { FISH_01: 0.9,NONE: 0.1  },
@@ -22,27 +22,51 @@ export const WaterData = [
         // obstacle:  { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 }
       }
     }
-    // ,
-    // {
-    //   id: 'WATER_02',
-    //   name: '湖泊',
-    //   description: '平静又开阔的的湖泊，适合长时间等待运气。',
-    //   habitats:{
-    //     backwater: '回水湾',
-    //     slowmoving: '缓流',
-    //     rapids: '急流',
-    //     shallow: '浅滩',
-    //     obstacle: '障碍'
-    //   },
-    //   image: '../../images/icons/rivers.png',
-    //   backgroundImage: '/images/icons/rivers.png',
-    //   fishProbabilities: {
-    //     backwater: { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
-    //     slowmoving: { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
-    //     rapids:    { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
-    //     shallow:   { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
-    //     obstacle:  { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 }
-    //   }
-    // }
+    ,
+    {
+      id: 'WATER_02',
+      name: '湖泊',
+      description: '平静又开阔的的湖泊，适合长时间等待运气。',
+      habitats:{
+        backwater: '回水湾',
+        slowmoving: '缓流',
+        rapids: '急流',
+        shallow: '浅滩',
+        obstacle: '障碍'
+      },
+      image: 'https://anglertest.xyz/game/BG/rivers.png',
+      backgroundImage: 'https://anglertest.xyz/game/BG/rivers.png',
+      fishProbabilities: {
+        backwater: { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
+        slowmoving: { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
+        rapids:    { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
+        shallow:   { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
+        obstacle:  { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 }
+      }
+    }
+
+    ,
+    {
+      id: 'WATER_03',
+      name: '城市河道',
+      description: '热门标点乃兵家必争之地，城市精灵的栖息地。',
+      habitats:{
+        backwater: '回水湾',
+        slowmoving: '缓流',
+        rapids: '急流',
+        shallow: '浅滩',
+        obstacle: '障碍'
+      },
+      image: 'https://anglertest.xyz/game/BG/city.png',
+      backgroundImage: 'https://anglertest.xyz/game/BG/city.png',
+      fishProbabilities: {
+        backwater: { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
+        slowmoving: { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
+        rapids:    { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
+        shallow:   { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 },
+        obstacle:  { FISH_01: 0.35, FISH_02: 0.25, NONE: 0.3 }
+      }
+    }
+
   ];
   
