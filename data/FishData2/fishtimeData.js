7841939114629b01b7a6e14f842d9090dadf770c
Fxm <fxm_cs@163.com>
2025-06-20 11:01:25 +0800

    钓鱼时间

--- data/FishData2/fishtimeData.js
+++ data/FishData2/fishtimeData.js
@@ -1,5 +1,5 @@
 export const fishtimeData = {
-  Basetime: 10,          // 初始钓鱼时间
-  everyfishon: -20,       // 每次中鱼或者脱钩后扣除
+  Basetime: 120,          // 初始钓鱼时间
+  everyfishon: -19,       // 每次中鱼或者脱钩后扣除
   everyNONE: -10          // 每次没有鱼咬钩扣除
 };
